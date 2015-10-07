e1c2dcf7b2acc38ffaa0f772069267073a2cffee
Daniel Alan Miller <dalan.miller@gmail.com>
2015-10-07 13:51:16 -0700

    Reinit

--- start_rethinkdb.sh
+++ start_rethinkdb.sh
@@ -5,11 +5,11 @@ ssh-add id_rackspace
 
 #nohup rethinkdb --bind all -d /mnt/rethinkdb_data -j 104.130.21.31 --cache-size 64000 &
 
-ssh -l root $1 bash -c "'echo 524240 > /proc/sys/vm/max_map_count'"
-ssh -l root $1 killall rethinkdb
+ssh -oStrictHostKeyChecking=no -l root $1 bash -c "'echo 524240 > /proc/sys/vm/max_map_count'"
+ssh -oStrictHostKeyChecking=no -l root $1 killall rethinkdb
 sleep 60
-ssh -l root $1 killall -9 rethinkdb
-ssh -l root $1 ./rethinkdb --bind all -d /mnt/rethinkdb_data -j 104.130.21.31 --cache-size 64000 --daemon
+ssh -oStrictHostKeyChecking=no -l root $1 killall -9 rethinkdb
+ssh -oStrictHostKeyChecking=no -l root $1 ./rethinkdb --bind all -d /mnt/rethinkdb_data -j 104.130.21.31 --cache-size 64000 --daemon
 
 
 
